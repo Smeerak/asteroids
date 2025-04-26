3d0fe777c36bd8c2877c2739a8e09d44c5e255fd
smeerak <kareem.elhawary@gmail.com>
2025-04-26 18:18:47 -0300

    added object groups

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-
+   
     #init Pygame
     pygame.init()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    #dynamically add a class variable
+    Player.containers=(updateable, drawable)
+
+    #now that variable is added, create an instance. It will automagically be added to the two groups.
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     ticker = pygame.time.Clock()
     delta_time = 0
@@ -24,16 +32,17 @@ def main():
         
         screen.fill(0)
 
+        #itterage over the two groups - update and draw
+        for u in updateable:
+            u.update(delta_time)
+        for d in drawable:
+            d.draw(screen)
 
-
-        #before we leave, we flip buffers
-        player.update(delta_time)
-        player.draw(screen)
+        #player.update(delta_time)
+        #player.draw(screen)
+        
         pygame.display.flip()
         delta_time = (ticker.tick(60))/1000
-
-
-
 
 if __name__ == "__main__":
     main()
